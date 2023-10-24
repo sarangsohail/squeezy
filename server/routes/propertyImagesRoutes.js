f45ccb64ebf3e5ec22be8ecd55336b413ef04328
sarang sohail <sarangsohail@gmail.com>
2023-10-24 16:04:18 +0800

    got the image loading working by adding .png to the file

--- server/routes/propertyImagesRoutes.js
+++ server/routes/propertyImagesRoutes.js
@@ -3,20 +3,32 @@ const router = express.Router();
 const multer = require('multer');
 const PropertyImageController = require('../controllers/PropertyImagesController');
 
-
-
 // Create a Multer storage configuration
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, '../frontend/public/property_images');
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now());
+      cb(null, file.fieldname + '-' + Date.now() + '.png');
+
     }
   });
   
   // Create a Multer instance with the storage configuration
-  const upload = multer({ storage: storage });
+  const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: 1000000 // Limit the file size to 1MB
+    },
+    fileFilter(req, file, cb) {
+      // Validate the file type using a regular expression
+      if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+        return cb(new Error('Please upload a valid image file'));
+      }
+      cb(undefined, true);
+    }
+  });  
+
   
   // Route for creating a property image
 router.post('/:propertyId', upload.single('image'), PropertyImageController.createPropertyImage);
@@ -29,3 +41,4 @@ router.delete('/:imageId', PropertyImageController.deletePropertyImage);
 
 
 module.exports = router;
+
